7df35a4f7e6bfc10a24898444aa3d394233b4317
turnbew <turnbew.co.uk@gmail.com>
2018-06-04 18:03:48 +0100

    Delete support_us_buttons.php

--- network-site/addons/default/modules/network_settings/views/content/partials/support_us_buttons.php
+++
@@ -1,4 +0,0 @@
-<div class="col-md-12" style="margin-top: 20px; margin-bottom: 20px;">
-	<button class="btn <?=$btnStyle?> toggle-display-on-support-us-page" id="btn_support_us_<?=$typeId?>" value="<?=$value?>"><?=$btnLabel?></button>
-	<?=sprintf(lang('support_us:text:toggling_parts'), $type, $btnStatus)?>                    
-</div>
